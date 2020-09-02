91abe1fa668aad260d79578d1e32721c1b631978
Rémi Coulon <remi.coulon@univ-rennes1.fr>
2020-09-02 10:02:25 +0200

    Simplifying Newton method in progress
    No more preliminary search for local minimum when when computing non-minimizing geodesics.

--- shaders/00aux.glsl
+++ shaders/00aux.glsl
@@ -56,6 +56,99 @@ const float NEWTON_TOLERANCE = 0.0001;
 // - COMPUTE THE EXACT DISTANCE
 // - COMPUTE THE DIRECTION OF THE SHORTEST GEODESIC FROM THE ORIGIN TO A GIVEN POINT
 
+
+// Auxiliary function.
+// The geodesics joining to origin to the point with cylindrical coordiantes (rho, *,z)
+// are in one-to-one correspondance with the zeros of chi (see paper)
+// We pass the argument rho as rho^2
+float chi(float rhoSq, float z, float phi) {
+    float res = -z + phi;
+
+    if (phi < 0.001) {
+        // when phi is close to zero we replace the next term by an asymptotic expansion
+        float phi2 = phi * phi;
+        float phi4 = phi2 * phi2;
+        res = res + rhoSq * phi * (phi2 + 30.) * (phi4 + 840.) / 302400.;
+    }
+    else {
+        res = res + 0.5 * rhoSq * (phi - sin(phi)) / pow(2. * sin(0.5 * phi), 2.0);
+    }
+    return res;
+}
+
+// Derivative of chi
+float dchi(float rhoSq, float z, float phi) {
+    float res = 1.;
+
+    if (phi < 0.001) {
+        // when phi is close to zero we replace the next term by an asymptotic expansion
+        float phi2 = phi * phi;
+        float phi4 = phi2 * phi2;
+        float phi6 = phi4 * phi2;
+        res = res + rhoSq * (25200. + 2520. * phi2 + 150. * phi4 + 7. * phi6) / 302400.;
+    }
+    else {
+        float cPhi = cos(0.5 * phi);
+        float sPhi = sin(0.5 * phi);
+        res =  res - rhoSq * (phi * cPhi - 2. * sPhi) / (8. * pow(sPhi, 3.));
+    }
+    return res;
+}
+
+
+// initialization of the Newthon method to find the zeros of chi
+// we look for a value of phi such that
+// * phiMin < phi < phiMax
+// * chi(phi) > 0
+// * dchi(phi) has the same as s
+float zero_chi_init(float rhoSq, float z, float phiMin, float phiMax, float s) {
+    float bdy;
+    if (s > 0) {
+        bdy = phiMax;
+    }
+    else {
+        bdy = phiMin;
+    }
+    float aux = 0.5 * phiMin + 0.5 * phiMax;
+    for (int i=0; i < MAX_NEWTON_INIT_ITERATION; i++){
+        if (sign(dchi(rhoSq, z, aux) == s) && chi(rhoSq, z, aux) > 0) {
+            break;
+        }
+        aux = 0.5 * aux + 0.5 * bdy;
+    }
+    return aux;
+}
+
+
+// find a zero phi of chi such that
+// * phiMin < phi < phiMax
+// * chi(phi) = 0 (of course !)
+// * dchi(phi) has the same as s
+// Use the Newton method.
+// if such a zero is found return true and update the value of phi
+// otherwise return false
+bool zero_chi(float rhoSq, float z, float phiMin, float phiMax, float s, out phi) {
+    float phi = zero_chi_init(rhoSq, z, phiMin, phiMax, s);
+    float aux;
+
+    for (int i=0; i < MAX_NEWTON_ITERATION; i++){
+        // backup of the previous value of phi
+        aux = phi;
+        // new value of phi
+        phi = phi - chi(rhoSq, z, phi) / dchi(rhoSq, z, phi);
+        if (phi < phiMin || phi > phiMax) {
+            return false;
+        }
+        if (sign(dchi(rhoSq, z, phi)) != s) {
+            return false;
+        }
+        if (abs(phi - aux) < NEWTON_TOLERANCE) {
+            return true;
+        }
+    }
+    return false;
+}
+
 // consider a geodesic gamma from the origin describing an angle phi
 // when reaching the point at distance rho of the axis (O,z)
 // return the value of z - z0, where z is the height of gamma at that point
@@ -213,7 +306,6 @@ float _dheight_newton(float rhosq, float z0, float phi0) {
     }
     return phi;
 }
-
 
 
 // return the two zeros (if they exit) of _height (seen as a function of phi)
