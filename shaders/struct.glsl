c9be30bfa7fa1feadcddc9243e2d0402b58fd70e
Rémi Coulon <remicoulon@univ-rennes1.fr>
2020-11-06 18:25:35 +0100

    trying the get Doxygen working for glsl

--- shaders/struct.glsl
+++ shaders/struct.glsl
@@ -1,9 +1,21 @@
-#version 120
+/**
+ * A simple 3x3 gaussian convolution filter, non-separated version
+ * @author Sebastian Schaefer
+ * @date 2012
+ * @namespace GLSL::FILTER::BLUR
+ * @class Gauss3x3
+ */
+#version 150 core
 
+uniform sampler2D image; ///< the input image
 
-struct Isometry {
-};
+in vec2 tex;    ///< texture coordinated
+out vec4 color; ///< the color output
 
-Isometry multiply(Isometry isom1, Isometry isom2) {
+/**
+ * The main routine: read the 3x3 neighbours and multiply with kernel
+ */
+void main()
+{
 
 }
