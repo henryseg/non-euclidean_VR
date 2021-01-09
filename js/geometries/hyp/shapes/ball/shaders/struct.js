9c4a633830c863a16e99a0090c6d7e9fafe491af
Rémi Coulon <remi.coulon@univ-rennes1.fr>
2021-01-09 21:51:25 +0100

    Fixed a bug in the UV map of a hyperbolic ball

--- js/geometries/hyp/shapes/ball/shaders/struct.js
+++ js/geometries/hyp/shapes/ball/shaders/struct.js
@@ -28,11 +28,24 @@ RelVector gradient(BallShape ball, RelVector v){
     return RelVector(negate(local), v.cellBoost, v.invCellBoost);
 }
 
+/**
+ * UV Map
+ * Note that the section of the (orthonormal) frame bundle that we use here is not invariant under isometries.
+ * Hence we have to go back and forth between the local and the global position.
+ * Find a better way to do this? 
+ */
 vec2 uvMap(BallShape ball, RelVector v){
-    Point center = applyIsometry(v.invCellBoost, ball.center);
-    Vector radius = direction(center, v.local.pos);
     Vector[3] f;
-    normalFrame(v.local.pos,f);
+    Point pos = applyGroupElement(v.cellBoost, v.local.pos);
+    normalFrame(pos, f);
+
+    f[0] = applyGroupElement(v.invCellBoost, f[0]);
+    f[1] = applyGroupElement(v.invCellBoost, f[1]);
+    f[2] = applyGroupElement(v.invCellBoost, f[2]);
+    Point center = applyIsometry(v.invCellBoost, ball.center);
+    
+    Vector radius = direction(center, v.local.pos);
+
     float x = geomDot(radius, f[0]);
     float y = geomDot(radius, f[1]);
     float cosPhi = geomDot(radius, f[2]);
