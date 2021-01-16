937c195b6c9dc46c03ec3b3b6d15f37165057996
Rémi Coulon <remi.coulon@univ-rennes1.fr>
2021-01-16 12:17:17 +0100

    Teleportations matrices in H3 written explicitly

--- js/geometries/hyp/groups/cube/set.js
+++ js/geometries/hyp/groups/cube/set.js
@@ -1,12 +1,10 @@
-import {Vector} from "../../geometry/General.js";
-import {Matrix4} from "../../../../lib/three.module.js";
 import {Teleportation} from "../../../../core/groups/Teleportation.js";
 import {TeleportationSet} from "../../../../core/groups/TeleportationSet.js";
 import {Group} from "../../../../commons/groups/isometry/Group.js";
 
 
-const cubeHalfWidth = 0.6584789485;
-const modelHalfCube = 0.5773502692;
+const sqrt3 = Math.sqrt(3);
+const modelHalfCube = 1 / sqrt3;
 
 const group = new Group();
 
@@ -84,23 +82,65 @@ const shiftYn = group.element();
 const shiftZp = group.element();
 const shiftZn = group.element();
 
-shiftXp.isom.makeTranslationFromDir(new Vector(-2 * cubeHalfWidth, 0, 0));
-shiftXn.isom.makeTranslationFromDir(new Vector(2 * cubeHalfWidth, 0, 0));
-shiftYp.isom.makeTranslationFromDir(new Vector(0, -2 * cubeHalfWidth, 0));
-shiftYn.isom.makeTranslationFromDir(new Vector(0, 2 * cubeHalfWidth, 0));
-shiftZp.isom.makeTranslationFromDir(new Vector(0, 0, -2 * cubeHalfWidth));
-shiftZn.isom.makeTranslationFromDir(new Vector(0, 0, 2 * cubeHalfWidth));
+shiftXp.isom.matrix.set(
+    2, 0, 0, -sqrt3,
+    0, 0, 1, 0,
+    0, -1, 0, 0,
+    -sqrt3, 0, 0, 2
+);
+shiftXn.isom.matrix.set(
+    2, 0, 0, sqrt3,
+    0, 0, -1, 0,
+    0, 1, 0, 0,
+    sqrt3, 0, 0, 2
+)
 
-//ADD ROTATIONS TO THE SIDE IDENTIFICATIONS TO MAKE INTO A MANIFOLD
-shiftXp.isom.matrix.multiply(new Matrix4().makeRotationX(Math.PI / 2).transpose());
-shiftXn.isom.matrix.multiply(new Matrix4().makeRotationX(-Math.PI / 2).transpose());
-shiftYp.isom.matrix.multiply(new Matrix4().makeRotationY(Math.PI / 2).transpose());
-shiftYn.isom.matrix.multiply(new Matrix4().makeRotationY(-Math.PI / 2).transpose());
-shiftZp.isom.matrix.multiply(new Matrix4().makeRotationZ(Math.PI / 2).transpose());
-shiftZn.isom.matrix.multiply(new Matrix4().makeRotationZ(-Math.PI / 2).transpose());
+shiftYp.isom.matrix.set(
+    0, 0, -1, 0,
+    0, 2, 0, -sqrt3,
+    1, 0, 0, 0,
+    0, -sqrt3, 0, 2
+);
+
+shiftYn.isom.matrix.set(
+    0, 0, 1, 0,
+    0, 2, 0, sqrt3,
+    -1, 0, 0, 0,
+    0, sqrt3, 0, 2
+);
+
+shiftZp.isom.matrix.set(
+    0, 1, 0, 0,
+    -1, 0, 0, 0,
+    0, 0, 2, -sqrt3,
+    0, 0, -sqrt3, 2
+)
+
+shiftZn.isom.matrix.set(
+    0, -1, 0, 0,
+    1, 0, 0, 0,
+    0, 0, 2, sqrt3,
+    0, 0, sqrt3, 2
+)
+
+// shiftXp.isom.makeTranslationFromDir(new Vector(-2 * cubeHalfWidth, 0, 0));
+// shiftXn.isom.makeTranslationFromDir(new Vector(2 * cubeHalfWidth, 0, 0));
+// shiftYp.isom.makeTranslationFromDir(new Vector(0, -2 * cubeHalfWidth, 0));
+// shiftYn.isom.makeTranslationFromDir(new Vector(0, 2 * cubeHalfWidth, 0));
+// shiftZp.isom.makeTranslationFromDir(new Vector(0, 0, -2 * cubeHalfWidth));
+// shiftZn.isom.makeTranslationFromDir(new Vector(0, 0, 2 * cubeHalfWidth));
+//
+// //ADD ROTATIONS TO THE SIDE IDENTIFICATIONS TO MAKE INTO A MANIFOLD
+// shiftXp.isom.matrix.multiply(new Matrix4().makeRotationX(Math.PI / 2).transpose());
+// shiftXn.isom.matrix.multiply(new Matrix4().makeRotationX(-Math.PI / 2).transpose());
+// shiftYp.isom.matrix.multiply(new Matrix4().makeRotationY(Math.PI / 2).transpose());
+// shiftYn.isom.matrix.multiply(new Matrix4().makeRotationY(-Math.PI / 2).transpose());
+// shiftZp.isom.matrix.multiply(new Matrix4().makeRotationZ(Math.PI / 2).transpose());
+// shiftZn.isom.matrix.multiply(new Matrix4().makeRotationZ(-Math.PI / 2).transpose());
 
 console.log(shiftXn.isom.matrix.toLog());
 console.log(shiftYn.isom.matrix.toLog());
+console.log(shiftZn.isom.matrix.toLog());
 
 const teleportXp = new Teleportation(testXp, glslTestXp, shiftXp, shiftXn);
 const teleportXn = new Teleportation(testXn, glslTestXn, shiftXn, shiftXp);
