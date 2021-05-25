08a4406733756162c01686d1e08371c5d93f8a5f
Rémi Coulon <remi.coulon@univ-rennes1.fr>
2021-05-25 21:49:22 +0200

    Fixed a bug in the mapping of the basic camera

--- js/core/cameras/basic/shaders/mapping.js
+++ js/core/cameras/basic/shaders/mapping.js
@@ -5,6 +5,7 @@ export default `//
  * @param[in] coords the coordinates of the point on the sphere
  */
 RelVector mapping(vec3 coords){
+    vec4 dir = vec4(coords, 0);
     dir = camera.matrix * dir;
     dir = normalize(dir);
     Vector v = createVector(ORIGIN, dir.xyz);
