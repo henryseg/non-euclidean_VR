b55111fb9b65b38f2d575fb71efad089504c445d
Rémi Coulon <remicoulon@univ-rennes1.fr>
2021-06-10 15:34:47 +0200

    Refactoring material location

--- js/commons/materials/astronomy/moon/MoonTexture.js
+++ js/commons/materials/astronomy/moon/MoonTexture.js
@@ -8,13 +8,14 @@ import {SimpleTextureMaterial} from "../../simpleTexture/SimpleTextureMaterial.j
  * @extends SimpleTextureMaterial
  *
  * @classdesc
- * Texture of the sun
- * Image from https://www.solarsystemscope.com/textures/
+ * Texture of the moon
+ * Image from https://svs.gsfc.nasa.gov/4720
+ * Credits : NASA's Scientific Visualization Studio
  */
-export class SunTexture extends SimpleTextureMaterial {
+export class MoonTexture extends SimpleTextureMaterial {
 
     constructor(thurstonJSPath) {
-        super(thurstonJSPath + '/commons/material/astronomy/sun/img/2k_sun.jpg', {
+        super(thurstonJSPath + '/commons/materials/astronomy/moon/img/lroc_color_poles_2k.png', {
             start: new Vector2(-Math.PI, 0),
             scale: new Vector2(1 / (2 * Math.PI), 1 / Math.PI),
         });
