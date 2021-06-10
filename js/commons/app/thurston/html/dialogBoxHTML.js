37d3a485a294aea6930fbdbf87839c2faae4f3a9
Rémi Coulon <remicoulon@univ-rennes1.fr>
2021-06-10 15:45:44 +0200

    Adding a help message

--- js/commons/app/thurston/html/dialogBoxHTML.js
+++ js/commons/app/thurston/html/dialogBoxHTML.js
@@ -2,6 +2,11 @@
 export default `
 <div id="thurstonDialogBoxWrap">
     <div id="thurstonDialogBox">
+    <p>
+        Choose the resolution of the picture, and click on the "Go".
+        You can download the image any time, with the "Download" button in the bottom right corner.
+        To leave the path tracer mode hit 'p'.
+    </p>
         <form action="javascript:void(0);">
             <label for="widthInput">Width :</label> <input id="widthInput" type="number"><br>
             <label for="heightInput">Height:</label> <input id="heightInput" type="number"><br>
