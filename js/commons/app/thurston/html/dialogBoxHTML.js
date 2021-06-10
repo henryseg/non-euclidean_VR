d3ce03a1acb7e497edc452663808a8280da6539b
Rémi Coulon <remicoulon@univ-rennes1.fr>
2021-06-10 15:46:25 +0200

    Correcting typo

--- js/commons/app/thurston/html/dialogBoxHTML.js
+++ js/commons/app/thurston/html/dialogBoxHTML.js
@@ -3,9 +3,9 @@ export default `
 <div id="thurstonDialogBoxWrap">
     <div id="thurstonDialogBox">
     <p>
-        Choose the resolution of the picture, and click on the "Go".
+        Choose the resolution of the picture, and click on "Go".
         You can download the image any time, with the "Download" button in the bottom right corner.
-        To leave the path tracer mode hit 'p'.
+        To leave the path tracer mode, hit 'p'.
     </p>
         <form action="javascript:void(0);">
             <label for="widthInput">Width :</label> <input id="widthInput" type="number"><br>
