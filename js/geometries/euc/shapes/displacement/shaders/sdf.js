d28742251f16d7bcbc25fdc707c765cc28758c8a
Rémi Coulon <remi.coulon@univ-rennes1.fr>
2021-06-11 19:16:56 +0200

    Playing a bit with displacement shape modifier

--- js/geometries/euc/shapes/displacement/shaders/sdf.js
+++ js/geometries/euc/shapes/displacement/shaders/sdf.js
@@ -4,10 +4,11 @@ export default `//
 * SDF for the displacement of a shape
 */
 float {{name}}_sdf(RelVector v){
+    
     float x = v.local.pos.coords.x;
     float y = v.local.pos.coords.y;
     float z = v.local.pos.coords.z;
-    float disp = sin(20. * x) * sin(20. * y) * sin(20. * z);
+    float disp = 0.05 * sin(30. * x) * sin(30. * y) * sin(30. * z);
     return {{shape.name}}_sdf(v) + disp;
 }
 `;
