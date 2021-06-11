9628d8ba32b42769543f5447c3dda20eae74009e
Rémi Coulon <remicoulon@univ-rennes1.fr>
2021-06-11 16:49:50 +0200

    First attempts of displacement operator on shape (continued)

--- js/geometries/euc/shapes/displacement/shaders/gradient.js
+++ js/geometries/euc/shapes/displacement/shaders/gradient.js
@@ -5,7 +5,17 @@ export default `//
 */
 RelVector {{name}}_gradient(RelVector v){
     RelVector gradient = {{shape.name}}_gradient(v);
-    vec4 dir = 
+
+    float x = v.local.pos.coords.x;
+    float y = v.local.pos.coords.y;
+    float z = v.local.pos.coords.z;
+    float disp = sin(20. * x) * sin(20. * y) * sin(20. * z);
+    vec4 dir = 20. * vec4(
+    cos(20. * x) * sin(20. * y) * sin(20. * z),
+    sin(20. * x) * cos(20. * y) * sin(20. * z),
+    sin(20. * x) * sin(20. * y) * cos(20. * z),
+    0
+    );
     Vector local = Vector(v.local.pos, n);
     RelVector disp = RelVector(local, v.cellBoost, v.invCellBoost)
     RelVector res = add(gradient, disp);
