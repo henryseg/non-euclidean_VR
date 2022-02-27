e8b1acf48a08e5fe782d56d65b9ea94ce65ca84c
Rémi Coulon <remi.coulon@univ-rennes1.fr>
2022-02-27 09:42:54 +0100

    Possibility to build the geometries separately

--- webpack.prod.all.js
+++ webpack.prod.all.js
@@ -5,13 +5,9 @@ const TerserPlugin = require('terser-webpack-plugin');
 // plugin used to copy files from 3dparty modules in a vendor directory
 const CopyPlugin = require("copy-webpack-plugin");
 
-const common = require('./webpack.common.js');
+const common = require('./webpack.prod.gen.js');
 
 module.exports = merge(common, {
-    mode: 'production',
-    experiments: {
-        outputModule: true,
-    },
     entry: {
         thurstonEuc: './src/thurstonEuc.js',
         thurstonHyp: './src/thurstonHyp.js',
@@ -21,39 +17,6 @@ module.exports = merge(common, {
         thurstonNil: './src/thurstonNil.js',
         thurstonSL2: './src/thurstonSL2.js',
         thurstonSol: './src/thurstonSol.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist/build/thurston'),
-        filename: '[name].js',
-        clean: true,
-        library: {
-            type: 'module',
-        },
-        environment: {
-             module: true,
-        },
-    },
-    externals: {
-        'three': 'three',
-        'webxr-polyfill': 'webxr-polyfill',
-        'stats': 'stats',
-        'dat.gui': 'dat.gui'
-    },
-    optimization: {
-        // splitChunks: {
-        //     chunks: 'all',
-        // },
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    format: {
-                        comments: false,
-                    },
-                },
-                extractComments: false,
-            }),
-        ],
     },
     plugins: [
         new CopyPlugin({
