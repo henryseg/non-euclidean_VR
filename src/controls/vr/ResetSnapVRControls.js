642e2fedb71e53ae86f9744d54f18267f3872478
Rémi Coulon <remi.coulon@univ-rennes1.fr>
2022-10-18 10:22:03 +0200

    Attempt snap on ResetVRControls

---
+++ src/controls/vr/ResetSnapVRControls.js
@@ -0,0 +1,83 @@
+import {
+    bind
+} from "../../utils.js";
+
+
+/**
+ * @class
+ *
+ * @classdesc
+ * When pressing the button, reset the position of the user (in the scene) to the default position (the origin).
+ * It can be used, when a VR experiment need be started at a very precise position (cf hexagon.html in H3)
+ */
+
+const RESET_CALLED = 1;
+const RESET_WAIT = 0;
+
+export class ResetSnapVRControls {
+
+    /**
+     * Constructor
+     * @param {RelPosition} position - the position in the geometry of the observer
+     * @param {Group} controller - the group representing the controller
+     * @param {boolean} alignFacing - option for updating the facing
+     *  - if False, the facing of the position is reset to its default value (quaternion = 1).
+     *  - if True, the facing is set up to that the camera is directed toward the negative z axis.
+     *    in this case the camera should be passed to the constructor as an argument
+     * @param {boolean} snap - if alignFacing and snap are true,
+     * align the orientation to the "closest" relation around the y-axis
+     * @param {VRCamera} camera - the camera (giving the position of the observer)
+     *
+     */
+    constructor(position, controller, alignFacing = false, snap = false, camera = undefined) {
+        this.position = position;
+        this.controller = controller;
+
+        this._reset = RESET_WAIT;
+        this._alignFacing = alignFacing;
+        this._snap = snap;
+        this._camera = camera;
+        if (this._alignFacing && camera === undefined) {
+            throw new Error("VRControlsReset.constructor, the camera is needed when the alignFacing option is on");
+        }
+
+        const _onSelectStart = bind(this, this.onSelectStart);
+        const _onSelectEnd = bind(this, this.onSelectEnd);
+
+        this.controller.addEventListener('selectstart', _onSelectStart);
+        this.controller.addEventListener('selectend', _onSelectEnd);
+    }
+
+    /**
+     * Event handler when the user starts selecting
+     */
+    onSelectStart() {
+        this._reset = RESET_CALLED;
+    }
+
+    /**
+     * Event handler when the user stops selecting
+     */
+    onSelectEnd() {
+    }
+
+    /**
+     * Function to update the position
+     */
+    update() {
+        if (this._reset === RESET_CALLED) {
+            this.position.reset();
+            if (this._alignFacing) {
+                const matrix = this._camera.threeCamera.matrixWorld;
+                this.position.local.quaternion.setFromRotationMatrix(matrix);
+                if (this._snap){
+                    this.position.local.quaternion.x = 0;
+                    this.position.local.quaternion.z = 0;
+                    this.position.local.quaternion.normalize();
+                }
+                this.position.local.quaternion.invert();
+            }
+            this._reset = RESET_WAIT;
+        }
+    }
+}
