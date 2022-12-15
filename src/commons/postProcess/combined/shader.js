bf8745f76347f37c84e4d5362f2ac577ea50d8ed
Rémi Coulon <remi.coulon@univ-rennes1.fr>
2022-12-15 21:42:09 +0100

    Preparing PostProcess customization

---
+++ src/commons/postProcess/combined/shader.js
@@ -0,0 +1,69 @@
+export default {
+
+    uniforms: {
+        'tDiffuse': {value: null}
+    },
+
+    // language=GLSL
+    vertexShader: `
+        varying vec2 vUv;
+        void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }`,
+
+    // language=GLSL
+    fragmentShader: `
+        uniform sampler2D tDiffuse;
+        varying vec2 vUv;
+
+
+        vec3 LessThan(vec3 f, float value)
+        {
+            return vec3(
+            (f.x < value) ? 1.0f : 0.0f,
+            (f.y < value) ? 1.0f : 0.0f,
+            (f.z < value) ? 1.0f : 0.0f);
+        }
+
+        //GAMMA CORRECTION
+        vec3 LinearToSRGB(vec3 rgb)
+        {
+            rgb = clamp(rgb, 0.0f, 1.0f);
+
+            return mix(
+            pow(rgb, vec3(1.0f / 2.4f)) * 1.055f - 0.055f,
+            rgb * 12.92f,
+            LessThan(rgb, 0.0031308f)
+            );
+        }
+        //TONE MAPPING
+        vec3 ACESFilm(vec3 x)
+        {
+            float a = 2.51f;
+            float b = 0.03f;
+            float c = 2.43f;
+            float d = 0.59f;
+            float e = 0.14f;
+            return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0.0f, 1.0f);
+        }
+
+        vec3 postProcess(vec3 pixelColor){
+
+            //set the exposure 
+            float exposure = 0.8;
+            pixelColor *= exposure;
+
+            //correct tones
+            pixelColor = ACESFilm(pixelColor);
+            pixelColor = LinearToSRGB(pixelColor);
+
+            return pixelColor;
+        }
+        
+        void main() {
+            vec4 color = texture2D(tDiffuse, vUv);
+            vec3 aux = postProcess(color.rgb);
+            gl_FragColor = vec4(min(vec3(1.0), aux), color.a);
+        }`
+};
