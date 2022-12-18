98974df39929b16fbfedc3ce166a22f4fa056966
Rémi Coulon <remi.coulon@univ-rennes1.fr>
2022-12-18 22:41:06 +0100

    Fixed a bug with texture material

--- src/commons/materials/astronomy/sun/SunTexture.js
+++ src/commons/materials/astronomy/sun/SunTexture.js
@@ -3,6 +3,7 @@ import {Vector2} from "three";
 import {SimpleTextureMaterial} from "../../simpleTexture/SimpleTextureMaterial.js";
 
 import texture from "./img/2k_sun.jpg";
+import struct from "./shaders/struct.glsl";
 
 /**
  * @class
@@ -19,5 +20,13 @@ export class SunTexture extends SimpleTextureMaterial {
             start: new Vector2(-Math.PI, 0),
             scale: new Vector2(1 / (2 * Math.PI), 1 / Math.PI),
         });
+    }
+
+    get uniformType() {
+        return 'SunTextureMaterial';
+    }
+
+    static glslClass() {
+        return struct;
     }
 }
