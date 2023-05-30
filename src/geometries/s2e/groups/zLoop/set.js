027f3a7b5f9c1bcf1a4952f298c5a740fdeb0cdc
Steve Trettel <stevejtrettel@gmail.com>
2023-05-30 12:52:23 -0700

    new multicolor material for s2e

--- src/geometries/s2e/groups/zLoop/set.js
+++ src/geometries/s2e/groups/zLoop/set.js
@@ -7,34 +7,34 @@ import {Vector} from "../../geometry/General.js"
 const group = new Group();
 
 function testWp(p) {
-    return p.coords.w > 1.25;
+    return p.coords.w > 0.75;
 }
 
 //language=GLSL
 const glslTestWp = `//
 
 bool testWp(Point p){
-    return p.coords.w>1.25;
+    return p.coords.w>0.75;
 }
 `;
 
 function testWn(p) {
-    return p.coords.w < -1.25;
+    return p.coords.w < -0.75;
 }
 
 //language=GLSL
 const glslTestWn = `//
 
 bool testWn(Point p){
-    return p.coords.w<-1.25;
+    return p.coords.w<-0.75;
 }
 `;
 
 const shiftWp = group.element();
 const shiftWn = group.element();
 
-shiftWp.isom.makeTranslationFromDir(new Vector(0, 0, -2.5));
-shiftWn.isom.makeTranslationFromDir(new Vector(0, 0, 2.5));
+shiftWp.isom.makeTranslationFromDir(new Vector(0, 0, -1.5));
+shiftWn.isom.makeTranslationFromDir(new Vector(0, 0, 1.5));
 
 export default new TeleportationSet()
     .add(testWp, glslTestWp, shiftWp, shiftWn)
