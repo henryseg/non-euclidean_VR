ae20898248dd6e9a2bcd40fe577e0aeaca398156
Steve Trettel <stevejtrettel@gmail.com>
2023-06-01 15:24:54 -0700

    multi material h2e

--- src/geometries/h2e/material/all.js
+++ src/geometries/h2e/material/all.js
@@ -1,2 +1,3 @@
 export {VaryingColorMaterial} from "./varyingColor/VaryingColorMaterial.js";
 export {QuotientGenus2Material} from "./quotientGenus2/QuotientGenus2Material.js";
+export {MultiColorMaterial} from "./multiColor/MultiColorMaterial.js";
